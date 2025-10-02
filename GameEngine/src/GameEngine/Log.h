485e29882832cfd1d5acbdbb0aa17229c3f0f0e9
Qczer <119138392+Qczer@users.noreply.github.com>
2025-10-03 00:23:00 +0200

    Added premake setup for windows

--- GameEngine/src/GameEngine/Log.h
+++ GameEngine/src/GameEngine/Log.h
@@ -20,21 +20,21 @@ namespace GameEngine {
 
 }
 
-// Core log macros 
-#define GE_CORE_ERROR(...)       ::GameEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define GE_CORE_WARN(...)        ::GameEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define GE_CORE_INFO(...)        ::GameEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define GE_CORE_TRACE(...)       ::GameEngine::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define GE_CORE_CRITICAL(...)    ::GameEngine::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#ifndef DIST_BUILD
+	// Core log macros 
+	#define GE_CORE_ERROR(...)       ::GameEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
+	#define GE_CORE_WARN(...)        ::GameEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+	#define GE_CORE_INFO(...)        ::GameEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
+	#define GE_CORE_TRACE(...)       ::GameEngine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+	#define GE_CORE_CRITICAL(...)    ::GameEngine::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
-// Client log macros 			 
-#define GE_ERROR(...)            ::GameEngine::Log::GetClientLogger()->error(__VA_ARGS__)
-#define GE_WARN(...)             ::GameEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define GE_INFO(...)             ::GameEngine::Log::GetClientLogger()->info(__VA_ARGS__)
-#define GE_TRACE(...)            ::GameEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define GE_CRITICAL(...)         ::GameEngine::Log::GetClientLogger()->critical(__VA_ARGS__)
-
-#ifdef DIST_BUILD
+	// Client log macros 			 
+	#define GE_ERROR(...)            ::GameEngine::Log::GetClientLogger()->error(__VA_ARGS__)
+	#define GE_WARN(...)             ::GameEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
+	#define GE_INFO(...)             ::GameEngine::Log::GetClientLogger()->info(__VA_ARGS__)
+	#define GE_TRACE(...)            ::GameEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
+	#define GE_CRITICAL(...)         ::GameEngine::Log::GetClientLogger()->critical(__VA_ARGS__)
+#else
 	#define GE_CORE_ERROR(...)   
 	#define GE_CORE_WARN(...)    
 	#define GE_CORE_INFO(...)    
---
+++ premake5.lua
@@ -0,0 +1,113 @@
+workspace "GameEngine"
+	architecture "x64"
+	startproject "Sandbox"
+	
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GameEngine"
+	location "GameEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++23"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GE_PLATFORM_WINDOWS",
+			"GE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPYFILE} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GE_DIST"
+		optimize "On"
+
+	filter "action:vs*"
+		buildoptions { "/utf-8" }
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"GameEngine/vendor/spdlog/include",
+		"GameEngine/src"
+	}
+
+	links
+	{
+		"GameEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++23"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GE_DIST"
+		optimize "On"
+
+	filter "action:vs*"
+		buildoptions { "/utf-8" }
