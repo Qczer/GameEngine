36d31d8dfecef821a55ee0ab4fe0ff4db599d701
Qczer <119138392+Qczer@users.noreply.github.com>
2025-10-04 04:25:52 +0200

    Added an Event System

--- GameEngine/src/GameEngine/Application.cpp
+++ GameEngine/src/GameEngine/Application.cpp
@@ -1,23 +1,30 @@
 #include "Application.h"
 
-#include <print>
-#include <iostream>
+#include "GameEngine/Events/ApplicationEvent.h"
+#include "GameEngine/Log.h"
 
 namespace GameEngine {
 
 	Application::Application()
 	{
-
 	}
 
 	Application::~Application()
 	{
-
 	}
 
 	void Application::Run() const
 	{
-		std::println("Hello, its Sandbox!");
+		WindowResizeEvent e(1280, 720);
+
+		if(e.isInCategory(EventCategoryApplication))
+		{
+			GE_TRACE(e); // gray
+		}
+		if(e.isInCategory(EventCategoryInput))
+		{
+			GE_INFO(e); // green
+		}
 	}
 
 }
---
+++ GameEngine/src/GameEngine/Events/KeyEvent.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "GameEngine/Events/Event.h"
+
+namespace GameEngine {
+
+	class GAME_ENGINE_API KeyEvent : public Event
+	{
+	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	protected:
+		KeyEvent(int keycode)
+			: m_KeyCode(keycode) {}
+
+		int m_KeyCode;
+	};
+
+	class GAME_ENGINE_API KeyPressedEvent : public KeyEvent
+	{
+	public:
+		KeyPressedEvent(int keycode, int repeatCount)
+			: KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override
+		{
+			return std::format("KeyPressed {} ({} repeats)", m_KeyCode, m_RepeatCount);
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed)
+	private:
+		int m_RepeatCount;
+	};
+
+	class GAME_ENGINE_API KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		KeyReleasedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			return std::format("KeyReleased {}", m_KeyCode);
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased)
+	};
+
+}
