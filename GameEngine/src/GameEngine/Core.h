e22adea313d48018b23a6d709ce0bd52b245d2fa
Qczer <119138392+Qczer@users.noreply.github.com>
2025-10-05 22:51:04 +0200

    Made asserts works and made premake md/mt configuration cross-platform

--- GameEngine/src/GameEngine/Core.h
+++ GameEngine/src/GameEngine/Core.h
@@ -10,6 +10,10 @@
 	#error Game Engine only supports Windows!
 #endif
 
+#ifdef GE_DEBUG
+	#define GE_ENABLE_ASSERTS
+#endif
+
 #ifdef GE_ENABLE_ASSERTS
 	#define GE_ASSERT(x, ...) { if(!(x)) { GE_ERROR("Assertion Failed: {}", __VA_ARGS__); __debugbreak(); } }
 	#define GE_CORE_ASSERT(x, ...) { if(!(x)) { GE_CORE_ERROR("Assertion Failed: {}", __VA_ARGS__); __debugbreak(); } }
