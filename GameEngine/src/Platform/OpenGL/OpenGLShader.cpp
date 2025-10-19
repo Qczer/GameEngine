e3c5bcc75238acf63acfe7be1b543c8f9a9f6b65
Qczer <119138392+Qczer@users.noreply.github.com>
2025-10-19 22:10:33 +0200

    Added Orthographic Camera

--- GameEngine/src/Platform/OpenGL/OpenGLShader.cpp
+++ GameEngine/src/Platform/OpenGL/OpenGLShader.cpp
@@ -1,7 +1,9 @@
 #include "gepch.h"
 #include "OpenGLShader.h"
 
-#include "glad/glad.h"
+#include <glad/glad.h>
+
+#include <glm/gtc/type_ptr.hpp>
 
 namespace GameEngine {
 
@@ -122,6 +124,12 @@ namespace GameEngine {
 	void OpenGLShader::Unbind() const
 	{
 		glUseProgram(0);
+	}
+
+	void OpenGLShader::UploadUniformMat4(const std::string& name, const glm::mat4& matrix)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
 	}
 
 }
