f4c79fdb38fa1ab0d7f4e313e808e289692a2089
Qczer <mateuszkuczera3@gmail.com>
2025-10-31 09:34:34 +0100

    Removed Input abstractions

--- GameEngine/src/Platform/Windows/WindowsInput.cpp
+++ GameEngine/src/Platform/Windows/WindowsInput.cpp
@@ -1,26 +1,26 @@
 #include "gepch.h"
-#include "Platform/Windows/WindowsInput.h"
+#include "GameEngine/Core/Input.h"
 
 #include "GameEngine/Core/Application.h"
 #include <GLFW/glfw3.h>
 
 namespace GameEngine {
 
-	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	bool Input::IsKeyPressed(int keycode)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, keycode);
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	bool Input::IsMouseButtonPressed(int button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, button);
 		return state == GLFW_PRESS;
 	}
 
-	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	std::pair<float, float> Input::GetMousePosition()
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xpos, ypos;
@@ -29,15 +29,15 @@ namespace GameEngine {
 		return { (float)xpos, (float)ypos };
 	}
 
-	float WindowsInput::GetMouseXImpl()
+	float Input::GetMouseX()
 	{
-		auto[x, y] = GetMousePositionImpl();
+		auto[x, y] = GetMousePosition();
 		return x;
 	}
 
-	float WindowsInput::GetMouseYImpl()
+	float Input::GetMouseY()
 	{
-		auto [x, y] = GetMousePositionImpl();
+		auto [x, y] = GetMousePosition();
 		return y;
 	}
 
